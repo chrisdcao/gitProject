ac644b83e53534602565a9e6a3aee6fcf629fef0
Khanh Cao <khanhcao96@gmail.com>
2020-10-20 11:37:24 +0700

    added one line

---
+++ khanh.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        This is the demo for Git
+                This is just the demo for git
+                This is just a demo for git
+}
