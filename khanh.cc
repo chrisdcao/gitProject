31f869e0ccf351940fb8e3306b281008bff31f63
Khanh Cao <khanhcao96@gmail.com>
2020-10-21 20:51:58 +0700

    added one fucking line

--- khanh.cc
+++ khanh.cc
@@ -1,12 +1,25 @@
 #include <iostream>
 using namespace std;
+adding one line 
 
+adding one fucking line
 int main() {
+int a = 10;
+int b = 20;
+while (x < 10) {[()]
 
+}
         This is the demo for Git
                 This is just the demo for git
                 This is just a demo for git
 }
+
+int main() {
+    int a = 10;
+    int x = 12;
+}
+
+
 This is a new one 
 #include <iostream>
 using namespace std;
